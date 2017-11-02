68e662b820d39bcb0ca61aede7493be614af6e2f
Dylan Pople <drpople14@gmail.com>
2017-11-02 13:24:01 -0600

    good working state, everything seems functional

--- main.rb
+++ main.rb
@@ -1,37 +1,70 @@
 require 'colorize'
+require 'pry'
 
 puts "Hello, welcome to the Middle Letter Word Sorter"
 
-def sort_this_shit_out_dot_exe
+def middle_letter_finder
   puts "Please enter a word that you'd like to find the middle letter of!"
   input = gets.strip.downcase
-  array = []
 
-  # TODO: figure out regex, put filter for multiple words
-  # if !input.include? " "
-  #   puts "Hey, thats more than one word. Please only put one word."
-  # else
-  #   puts input
-  # end
+  if input[/\s/]
+    puts "Hey, that looks like more than one word to me. \nJust write one word."
+    middle_letter_finder
 
-  if input.length % 2 != 0
+  elsif input.length <= 2
+    puts "Really? You need the middle letters for that?"
+    print "#{input.capitalize}".colorize(:yellow)
+    print " is already the middle of "
+    puts "#{input}!".colorize(:yellow)
+    puts "Pick a better word."
+    middle_letter_finder
+
+  elsif input.length % 2 != 0
     size = input.length
     puts "This word is odd, cool, you'll get back one letter."
-    print "Calculating..."
-    array = input.chars.to_a.each_slice(1).to_a.map {|s| s.to_s }
-    puts "done!"
-    print "The middle letter for #{input} is: #{array[(size - 1)/2]}."
-    puts " Impossible task accomplished!"
+    print "Calculating...".colorize(:light_blue)
+    array = input.chars.each_slice(1).map { |s| s.to_s }
+    middle_letter = array[(size-1)/2][2]
+    puts "done!".colorize(:green)
+    print "The middle letter for "
+    print "#{input}".colorize(:yellow)
+    print " is: "
+    print "#{middle_letter}".colorize(:yellow)
+    puts ". Impossible task accomplished!"
+    puts "Would you like to find more middle letters of words?"
+    done = gets.strip.downcase
+    if done == "yes"
+      middle_letter_finder
+    elsif done == "no" || done == "quit"
+      print "Shutting down...".colorize(:red)
+      puts "Goodbye"
+      exit
+    end
+
 
   else
     size = input.length
     puts "This word is even, ok, you'll get back two letters."
-    print "Calculating..."
-
-    puts "done!"
-    print "The middles letters for #{input} are: blahblahblah"
+    print "Calculating...".colorize(:light_blue)
+    array = input.chars.each_slice(1).map { |s| s.to_s }
+    middle_letters = array[(size/2)-1][2] + array[size/2][2]
+    puts "done!".colorize(:green)
+    print "The middle letters for "
+    print "#{input}".colorize(:yellow)
+    print " are: "
+    print "#{middle_letters}".colorize(:yellow)
+    puts ". Impossible task accomplished!"
+    puts "Would you like to find more middle letters of words?"
+    done = gets.strip.downcase
+    if done == "yes"
+      middle_letter_finder
+    elsif done == "no" || done == "quit"
+      print "Shutting down...".colorize(:red)
+      puts "Goodbye"
+      exit
+    end
   end
 
 end
 
-sort_this_shit_out_dot_exe
+middle_letter_finder
